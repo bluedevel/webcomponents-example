e16ce74266dadc2ed4ff9f6bf11dd6d00670a442
bluedevel <eurob@1896>
2017-08-15 09:04:04 +0200

    load template in custom element

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 require('@webcomponents/webcomponentsjs/webcomponents-hi-sd-ce');
 console.log("Loaded @webcomponents/webcomponentsjs");
 
+require('./app.html');
 require('./my-element');
 
+console.log(window.customElements);
 
-document.querySelector('body').innerHTML = `
-<p>Test!!!</p>
-<my-element>
-    <test></test>    
-</my-element>
-`;
+document.querySelector('body').innerHTML = '<test-app></test-app>';
